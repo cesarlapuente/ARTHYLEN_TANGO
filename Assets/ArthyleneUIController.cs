de8464b6209641b1bcd526b86869dc30bd289a50
Damien Brun <makowildcat@gmail.com>
2017-08-31 12:43:24 +0200

    Create UIController (GameObject and script) to handle menu event (such as buttons, show/hide menu)

---
+++ Assets/ArthyleneUIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArthyleneUIController : MonoBehaviour 
+{
+	public GameObject m_panelMainMenu;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	// 
+	public void StartPlace()
+	{
+		m_panelMainMenu.SetActive(false);
+	}
+}
