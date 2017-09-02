8d7d06bb10b75ac6f6e6591d2d0628d75cf32a1d
Damien Brun <makowildcat@gmail.com>
2017-09-02 18:13:01 +0200

    Create ProduceData script (serialize/deserialize produce to/from xml)

---
+++ Assets/Arthylene/Scripts/ProduceData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Xml.Serialization;
+
+/// <summary>
+/// Data container for produce.
+/// 
+/// Used for serializing/deserializing produce to xml.
+/// </summary>
+[System.Serializable]
+public class ProduceData
+{
+	/// <summary>
+	/// Produce's type.
+	/// (e.g. banana, apple, watermelon...).
+	/// </summary>
+	[XmlElement("type")]
+	public int m_type;
+
+	/// <summary>
+	/// Position of the this produce, with respect to the origin of the game world (i.e. the produce department).
+	/// </summary>
+	[XmlElement("position")]
+	public Vector3 m_position;
+
+	/// <summary>
+	/// Rotation of the this produce.
+	/// </summary>
+	[XmlElement("orientation")]
+	public Quaternion m_orientation;
+}
